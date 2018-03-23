6123629bc451852a1a49e0cc7c7c5418a3ce3b20
MASA <miguel7suarez7@gmail.com>
2018-03-23 12:32:22 +0100

    Sección 12 Terminada con el backend

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+// Google
+module.exports.GOOGLE_CLIENT_ID = '422860089267-f1e2tqrb0ksvpojr6ribh9qr310jec0d.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'hfI_8jpXjVAJ9mvhkwwDU06l';
